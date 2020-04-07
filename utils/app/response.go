5414580ad3586f8d1c73465830dbf96da0f77d5a
fajar septiawan <fajarsep12dev@gmail.com>
2020-04-07 20:00:59 +0700

    add obj

---
+++ utils/app/response.go
@@ -0,0 +1,27 @@
+package app
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+// Gin Struct type
+type Gin struct {
+	C *gin.Context
+}
+
+// Response Struct type obj
+type Response struct {
+	Code int         `json:"code"`
+	Msg  string      `json:"msg"`
+	Data interface{} `json:"data"`
+}
+
+// Response setting gin.JSON
+func (g *Gin) Response(httpCode, errCode int, data interface{}) {
+	g.C.JSON(httpCode, Response{
+		Code: httpCode,
+		Msg:  GetMsg(errCode),
+		Data: data,
+	})
+	return
+}
